42bc0867e3a33f780ef96e32503f565afaaaebe4
janani1998 <41136718+janani1998@users.noreply.github.com>
2018-07-20 11:49:32 +0530

    Create first5multiple.java

---
+++ first5multiple.java
@@ -0,0 +1,12 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n=8,a;
+		for(int i=1;i<=5;i++)
+		{
+			a=i*n;
+			System.out.print(a+" ");
+		}
+	}
+}
