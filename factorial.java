50aeabd4966dab69e6fd8a7fa7154502fa5e0a6f
janani1998 <41136718+janani1998@users.noreply.github.com>
2018-07-20 11:36:12 +0530

    Create factorial.java

---
+++ factorial.java
@@ -0,0 +1,12 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n=7,i,j;
+			for( j=2,i=1;j<=7;j++)
+			{
+				i=i*j;
+			}
+		System.out.print(i);
+	}
+}
